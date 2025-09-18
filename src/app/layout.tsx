91e7be285b2e1f0836e17f746ae42ce0a57c84ca
Aymeric LEGER ACHARD <aymeric.leger-achard@3310street.com>
2025-09-18 17:31:20 +0200

    Ajout Google Analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -29,20 +29,6 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="fr" className={`${questrial.className} ${poppins.variable}`}>
-      <head>
-	      <Script
-		      id={"google-tag-manager-script"}
-		      dangerouslySetInnerHTML={{
-			      __html: `
-              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-              })(window,document,'script','dataLayer','GTM-5ZS3MVLG');
-            `,
-		      }}
-	      />
-      </head>
       <body>
 	      <noscript
 		      dangerouslySetInnerHTML={{
@@ -50,6 +36,17 @@ export default function RootLayout({
 	            height="0" width="0" style="display:none;visibility:hidden"></iframe>`
 		      }}
 	      />
+
+	      <Script id="google-tag-manager" strategy="afterInteractive">
+		      {`
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-5ZS3MVLG');
+          `}
+	      </Script>
+
         <NavigationComponent />
         <div className="superContainer">
 	        {children}
